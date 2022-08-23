0b9e72843f58a7e2643a4ee494daada8515b8d7a
Jalomo <L20070518@cdmadero.tecnm.mx>
2022-08-23 01:22:40 -0500

    First Commit

---
+++ API/Data/Migrations/20220823055615_SecondCreation.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class SecondCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "Username");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "UserName");
+        }
+    }
+}
